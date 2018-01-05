d63210b4c1b2dfb9b385da4f849cb6d621976773
nico24687 <theonlynico@hotmail.com>
2018-01-05 09:54:47 -0700

    clean up

--- spec/models/machine_snack_spec.rb
+++ spec/models/machine_snack_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe MachineSnack, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "belongs to a snack and a machine" do 
+    machinesnack = MachineSnack.create(machine_id: 1, snack_id: 1 )
+
+    expect(machinesnack).to respond_to(:snack)
+    expect(machinesnack).to respond_to(:machine)
+  end
 end
