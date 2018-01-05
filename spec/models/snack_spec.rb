9b45f25ff1bab329d252a000ce426480bfa42568
nico24687 <theonlynico@hotmail.com>
2018-01-05 09:50:07 -0700

    model test for a snack

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Snack, type: :model do
   it "has many machines" do 
-    snack = Snack.create(name: "oreos", )
+    snack = Snack.create(name: "oreos", price: 200)
+
+    expect(snack).to respond_to(:machines)
   end 
 end
