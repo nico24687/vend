387003a5e4732fa7058e4703f315109c41328f12
nico24687 <theonlynico@hotmail.com>
2018-01-05 09:14:31 -0700

    first test passing

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -10,3 +10,16 @@ feature 'When a user visits a vending machine show page' do
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
 end
+
+feature 'When a user visits a vending machine show page' do
+  scenario 'they see the name of all the snacks associated with that machine' do
+    owner = Owner.create(name: "Nico")
+    machine  = owner.machines.create(location: "Tennis club")
+    snack = machine.snacks.create(name: "Cheetos", price: 300)
+
+    visit machine_path(machine)
+   
+    expect(page).to have_content("Tennis club")
+    expect(page).to have_content("Cheetos:$3.00")
+  end
+end
